49a25c2ef8c6069a3957e0f1f59f265e5d2c2b0c
starminalush <izy@intersvyaz.net>
2023-11-25 13:08:45 +0500

    some code refactoring

---
+++ notebooks/yolov8/src/postprocessing.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+from utils import multiclass_nms, xywh2xyxy
+
+
+def process_output(output, conf_threshold, iou_threshold, input_width, input_height, img_width, img_height):
+    predictions = np.squeeze(output[0]).T
+
+    # Filter out object confidence scores below threshold
+    scores = np.max(predictions[:, 4:], axis=1)
+    predictions = predictions[scores >= conf_threshold, :]
+    scores = scores[scores > conf_threshold]
+
+    if len(scores) == 0:
+        return [], [], []
+
+    # Get the class with the highest confidence
+    class_ids = np.argmax(predictions[:, 4:], axis=1)
+
+    # Get bounding boxes for each object
+    boxes = extract_boxes(predictions, input_width, input_height, img_width, img_height)
+
+    # Apply non-maxima suppression to suppress weak, overlapping bounding boxes
+    # indices = nms(boxes, scores, self.iou_threshold)
+    indices = multiclass_nms(boxes, scores, class_ids, iou_threshold)
+
+    return boxes[indices], scores[indices], class_ids[indices]
+
+
+def extract_boxes(predictions, input_width, input_height, img_width, img_height):
+    # Extract boxes from predictions
+    boxes = predictions[:, :4]
+
+    # Scale boxes to original image dimensions
+    boxes = rescale_boxes(boxes, input_width, input_height, img_width, img_height)
+
+    # Convert boxes to xyxy format
+    boxes = xywh2xyxy(boxes)
+
+    return boxes
+
+
+def rescale_boxes(boxes, input_width, input_height, img_width, img_height):
+    # Rescale boxes to original image dimensions
+    input_shape = np.array([input_width, input_height, input_width, input_height])
+    boxes = np.divide(boxes, input_shape, dtype=np.float32)
+    boxes *= np.array([img_width, img_height, img_width, img_height])
+    return boxes
---
+++ notebooks/yolov8/src/preprocessing.py
@@ -0,0 +1,54 @@
+import numpy as np
+import cv2
+
+
+def _normalize(image: np.ndarray) -> np.ndarray:
+    return image / 255.0
+
+
+def _resize_with_padding(image: np.ndarray, new_image_size=(640, 480), fill_color=(0, 0, 0)) -> np.ndarray:
+    image_height, image_width, *_ = image.shape
+    new_image_width, new_image_height = new_image_size
+    ratio = min(new_image_width / image_width, new_image_height / image_height)
+
+    scaled_image_width = int(round(image_width * ratio))
+    scaled_image_height = int(round(image_height * ratio))
+    if (image_width, image_height) == (scaled_image_width, scaled_image_height):
+        return image
+
+    image = cv2.resize(
+        image,
+        (scaled_image_width, scaled_image_height),
+        interpolation=cv2.INTER_LINEAR,
+    )
+
+    width_padding = (new_image_width - scaled_image_width) / 2
+    height_padding = (new_image_height - scaled_image_height) / 2
+
+    top, bottom = int(round(height_padding - 0.1)), int(round(height_padding + 0.1))
+    left, right = int(round(width_padding - 0.1)), int(round(width_padding + 0.1))
+
+    return cv2.copyMakeBorder(
+        image, top, bottom, left, right, cv2.BORDER_CONSTANT, value=fill_color
+    )
+
+
+def _transpose(image: np.ndarray) -> np.ndarray:
+    return image.transpose(2, 0, 1)
+
+
+def _add_batch(image: np.ndarray) -> np.ndarray:
+    return image[np.newaxis, :, :, :].astype(np.float32)
+
+
+def preprocess_image(image: np.ndarray, image_size: tuple[int, int]):
+    img_height, img_width = image.shape[:2]
+
+    input_img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    input_img = _normalize(input_img)
+    input_img = _resize_with_padding(image=input_img, new_image_size=image_size)
+    input_img = _transpose(input_img)
+    input_img = _add_batch(input_img)
+
+    return input_img, img_width, img_height
